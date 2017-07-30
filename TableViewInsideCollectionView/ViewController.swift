9b47fce11307732333523c6620cd09f74e1519c3
Vijay Pattur Mohankumar <upattvi@bomasml-h6lvg8w.fios-router.home>
2017-07-30 14:54:35 -0400

    Initial Commit

---
+++ TableViewInsideCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewInsideCollectionView
+//
+//  Created by Pattur Mohankumar, Vijay Prasath on 7/30/17.
+//  Copyright © 2017 Pattur Mohankumar, Vijay Prasath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
